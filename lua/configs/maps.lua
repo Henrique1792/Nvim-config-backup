95e834780e8384296af47f7c5243ea64e2c35c44
Henrique Freitas <freitash1792@gmail.com>
2022-09-07 16:16:31 -0300

    maps without plugins added - we'll have to set them up before moving on

--- lua/configs/maps.lua
+++ lua/configs/maps.lua
@@ -1,4 +1,15 @@
 -- keymaps
+-- Writer sidenote - this mappings run just using vim structure
+-- plugins have their own place for mapping and will be set
+-- individually
+--
+--
+-- e.g
+--
+-- Map('n','<F5>,':Gvdiffsplit!<CR>'<silent>  :Gvdiffsplit!<CR>)
+-- Map('n','<F6>,':only!<CR><C-[>:SignifyEnable<CR>'<silent>  :only!<CR><C-[>:SignifyEnable<CR>)
+-- will be transfered to plugin setup
+
 local keymap = vim.keymap
 
 -- trying kindly to remove verbosing
@@ -23,10 +34,23 @@ Map('n','<F3>',':noh<CR>',{silent=true})
 Map('n','<F2>',':tabnew<CR>',{silent=true})
 Map('n','<F4>',':tabclose<CR>',{silent=true})
 
--- Map('n','<F5>,':Gvdiffsplit!<CR>'<silent>  :Gvdiffsplit!<CR>)
--- Map('n','<F6>,':only!<CR><C-[>:SignifyEnable<CR>'<silent>  :only!<CR><C-[>:SignifyEnable<CR>)
+-- We'll fix register behaviour later.
+-- Map('n','<F7>',':let @+=expand("%:p")<CR><C-[>:let @*=expand("%:p")<CR>', {noremap=true, silent=true})
+-- Map('n','<F8>',':let @+=expand("%:p:h")<CR><C-[>:let @*=expand("%:p:h")<CR>', {noremap=true, silent=true})
+
+Map('n','<leader>q',':<C-W>q<CR>',{silent=true})
+Map('n','<leader>\\',':vsplit<CR><C-[><C-W>l',{silent=true})
+Map('n','<leader>-',':split<CR><C-[><C-W>j',{silent=true})
+
+Map('n','<leader>h',':<C-W>h',{silent=true})
+Map('n','<leader>j',':<C-W>j',{silent=true})
+Map('n','<leader>k',':<C-W>k',{silent=true})
+Map('n','<leader>l',':<C-W>l',{silent=true})
 
 
--- Map('n','<F7>,''<silent>  :let @+=expand("%:p")<CR><C-[>:let @*=expand("%:p")<CR>)
--- Map('n','<F8>,''<silent>  :let @+=expand("%:p:h")<CR><C-[>:let @*=expand("%:p:h")<CR>)
+Map('n','<localleader><Left>',':<C-W><',{silent=true})
+Map('n','<localleader><Down>',':<C-W>-',{silent=true})
+Map('n','<localleader><Up>',':<C-W>+',{silent=true})
+Map('n','<localleader><Right>',':<C-W>>',{silent=true})
+Map('n','<localleader>r ','<C-[>:reg<CR>',{silent=true})
 
