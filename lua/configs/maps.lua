f9b04337f313a8821a21a3be582a4224e42e15bb
Henrique Freitas <freitash1792@gmail.com>
2022-09-07 15:57:33 -0300

    added proper map structure - applying for every entry now

--- lua/configs/maps.lua
+++ lua/configs/maps.lua
@@ -1,8 +1,9 @@
 -- keymaps
 local keymap = vim.keymap
 
-function Map(Mode, sequence, command, params)
-	keymap.set{Mode, sequence, command, params}
+-- trying kindly to remove verbosing
+function Map(mode, sequence, command, params)
+	keymap.set(mode, sequence, command, params)
 end
 
 
@@ -15,20 +16,17 @@ Map('n','<localleader>o','<C-[>:bp<CR>',{noremap=true, silent=true})
 Map('n','<localleader>d','<C-[>:bd<CR>',{noremap=true, silent=true})
 Map('n','<localleader>W','<C-[>:bw!<CR>',{noremap=true, silent=true})
 Map('n','<localleader>w','<C-[>:w<CR>',{noremap=true, silent=true})
-Map('n','<leader>sv','',{noremap=true, silent=true})
-Map('n','<leader>ev','',{noremap=true, silent=true})
-Map('n','<leader>sv','',{noremap=true, silent=true})
 
 
 
-Map('n','<F3>,':noh<CR>'<silent>  :noh<CR>)
-Map('n','<F2>,':tabnew<CR>'<silent>  :tabnew<CR>)
-Map('n','<F4>,':tabclose<CR>'<silent>  :tabclose<CR>)
+Map('n','<F3>',':noh<CR>',{silent=true})
+Map('n','<F2>',':tabnew<CR>',{silent=true})
+Map('n','<F4>',':tabclose<CR>',{silent=true})
 
 -- Map('n','<F5>,':Gvdiffsplit!<CR>'<silent>  :Gvdiffsplit!<CR>)
 -- Map('n','<F6>,':only!<CR><C-[>:SignifyEnable<CR>'<silent>  :only!<CR><C-[>:SignifyEnable<CR>)
 
 
-Map('n','<F7>,''<silent>  :let @+=expand("%:p")<CR><C-[>:let @*=expand("%:p")<CR>)
-Map('n','<F8>,''<silent>  :let @+=expand("%:p:h")<CR><C-[>:let @*=expand("%:p:h")<CR>)
+-- Map('n','<F7>,''<silent>  :let @+=expand("%:p")<CR><C-[>:let @*=expand("%:p")<CR>)
+-- Map('n','<F8>,''<silent>  :let @+=expand("%:p:h")<CR><C-[>:let @*=expand("%:p:h")<CR>)
 
