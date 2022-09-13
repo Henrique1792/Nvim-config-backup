88f4ce40bf9f345e87bd0c2d1c3c1986de3e60a8
Henrique Freitas <freitash1792@gmail.com>
2022-09-13 19:10:23 -0300

    changed buffer behaviour on lualine

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -7,7 +7,7 @@ lualine.setup {
     sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'buffers'},
+        lualine_c = {{'buffers',mode=4}},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress', 'location'},
         lualine_z = {'filename'}
